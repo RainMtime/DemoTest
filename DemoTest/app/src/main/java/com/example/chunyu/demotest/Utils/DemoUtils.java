8d3b1338c16d30b2f8837f04df3844ae76cccd8f
chunyu <jcy23401@163.com>
2017-06-16 15:14:13 +0800

    提交io操作文件

--- DemoTest/app/src/main/java/com/example/chunyu/demotest/Utils/DemoUtils.java
+++ DemoTest/app/src/main/java/com/example/chunyu/demotest/Utils/DemoUtils.java
@@ -1,7 +1,17 @@
 package com.example.chunyu.demotest.Utils;
 
 import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
 import android.view.WindowManager;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 /**
  * Created by chunyu on 2017/6/14.
@@ -9,6 +19,7 @@ import android.view.WindowManager;
 
 public final class DemoUtils {
 
+    private static final String TAG = "DemoUtils";
 
     public static int getScreenWidth(Context context) {
         WindowManager wm = (WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
@@ -18,6 +29,84 @@ public final class DemoUtils {
     public static int getScreenHeight(Context context) {
         WindowManager wm = (WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         return wm.getDefaultDisplay().getHeight();
+    }
+
+
+    public static boolean copyFile(File srcFile, File destFile) {
+
+        try {
+            long start = System.currentTimeMillis();
+            FileInputStream fileInputStream = new FileInputStream(srcFile);
+            FileOutputStream fileOutputStream = new FileOutputStream(destFile);
+            byte[] bytes = new byte[1024];
+            int num = 0;
+            while ((num = fileInputStream.read(bytes)) != -1) {
+                fileOutputStream.write(bytes, 0, num);
+                fileOutputStream.flush();
+            }
+            fileInputStream.close();
+            fileOutputStream.close();
+            long end = System.currentTimeMillis();
+            Log.i(TAG, "copyFiletime: " + (end - start));
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "copyFile: exception:" + e.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean copyFilebyBuffer(File srcFile, File destFile) {
+        try {
+            long start = System.currentTimeMillis();
+            FileInputStream fileInputStream = new FileInputStream(srcFile);
+            FileOutputStream fileOutputStream = new FileOutputStream(destFile);
+            BufferedInputStream bufInputStream = new BufferedInputStream(fileInputStream);
+            BufferedOutputStream bufOutputStream = new BufferedOutputStream(fileOutputStream);
+            int num = 0;
+            while ((num = bufInputStream.read()) != -1) {
+                Log.e(TAG, "copyFilebyBuffer->num:" + num);
+                bufOutputStream.write(num);
+                bufOutputStream.flush();
+            }
+            fileInputStream.close();
+            fileOutputStream.close();
+            long end = System.currentTimeMillis();
+            Log.i(TAG, "copyFilebyBuffer: " + (end - start));
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "copyFilebyBuffer: exception:" + e.getMessage());
+            return false;
+        }
+
+    }
+
+
+    public static boolean copyFilebyWriter(File srcFile, File destFile) {
+        try {
+            long start = System.currentTimeMillis();
+            FileReader fileReader = new FileReader(srcFile);
+            FileWriter fileWriter = new FileWriter(destFile);
+            int num = 0;
+            char[] chars = new char[1024];
+            while ((num = fileReader.read(chars)) != -1) {
+                Log.e(TAG, "copyFilebyWriter->num:" + num);
+                fileWriter.write(chars, 0, num);
+                fileWriter.flush();
+            }
+            fileReader.close();
+            fileWriter.close();
+            long end = System.currentTimeMillis();
+            Log.i(TAG, "copyFilebyWriter: " + (end - start));
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "copyFilebyWriter: exception:" + e.getMessage());
+            return false;
+        }
+    }
+
+
+    public static String getInnerSDCardPath() {
+        return Environment.getExternalStorageDirectory().getPath();
     }
 
 }
