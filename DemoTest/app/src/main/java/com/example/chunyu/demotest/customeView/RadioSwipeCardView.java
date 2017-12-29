8119b0ea1f1700801bce7a0703dc277271942661
chunyu <jcy23401@163.com>
2017-12-29 16:40:02 +0800

    提交CardView逻辑代码。

--- DemoTest/app/src/main/java/com/example/chunyu/demotest/customeView/RadioSwipeCardView.java
+++ DemoTest/app/src/main/java/com/example/chunyu/demotest/customeView/RadioSwipeCardView.java
@@ -7,12 +7,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -25,6 +27,8 @@ import com.example.chunyu.demotest.Utils.DemoUtils;
  */
 
 public class RadioSwipeCardView extends RelativeLayout {
+
+    private static final float BOTTOM_VIEW_ROTATION = -5.0f;
 
     private float TouchX;
     private float TouchY;
@@ -43,8 +47,8 @@ public class RadioSwipeCardView extends RelativeLayout {
     private View mBottomCardView;
     private View mTopCardView;
     private ViewGroup decorView;
-
     private boolean mNeedIntercept = false;
+
 
     public RadioSwipeCardView(Context context) {
         super(context);
@@ -90,8 +94,7 @@ public class RadioSwipeCardView extends RelativeLayout {
                         decorView = getDecorView(getContext());
                     }
                 }
-                if (mTopCardView == null) {
-                    mTopCardView = retriveTopCardView();
+                if (mTopCardViewX == -1) {
                     int[] xy = new int[2];
                     mTopCardView.getLocationOnScreen(xy);
                     mTopCardViewX = xy[0];
@@ -117,13 +120,12 @@ public class RadioSwipeCardView extends RelativeLayout {
 
                 float dealtX = event.getRawX() - lastX;
                 float dealtY = event.getRawY() - lastY;
-                if (!hasFloatingView) {
-                    mTopCardView = retriveTopCardView();
+                if (!hasFloatingView && Math.abs(sumDistanceX) > ViewConfiguration.getTouchSlop()) {
                     addViewToDecorView(mTopCardView);
                 }
 
                 if (hasFloatingView) {
-                    translateView(dealtX, dealtY, sumDistanceX, sumDistanceY);
+                    changeViewProperty(dealtX, dealtY, sumDistanceX, sumDistanceY);
                     comsumed = true;
                 }
 
@@ -140,7 +142,6 @@ public class RadioSwipeCardView extends RelativeLayout {
             default:
                 break;
         }
-
         return comsumed;
     }
 
@@ -155,7 +156,8 @@ public class RadioSwipeCardView extends RelativeLayout {
         mTopCardView = createTopCardView();
         addView(mBottomCardView);
         addView(mTopCardView);
-        mBottomCardView.setRotation(-5);
+        mBottomCardView.setRotation(BOTTOM_VIEW_ROTATION);
+        mBottomCardView.setAlpha(0.0f);
     }
 
     protected View createTopCardView() {
@@ -185,8 +187,8 @@ public class RadioSwipeCardView extends RelativeLayout {
     }
 
 
-    public void translateView(float x, float y, float sumX, float sumY) {
-        Log.i("chunyu-translateView", "x:" + x + "\t y:" + y + "\tsumX:" + sumX + "\tsumY:" + sumY);
+    public void changeViewProperty(float x, float y, float sumX, float sumY) {
+        Log.i("chunyu-change", "x:" + x + "\t y:" + y + "\tsumX:" + sumX + "\tsumY:" + sumY);
         if (floatingView != null && hasFloatingView) {
             float translationX = floatingView.getTranslationX();
             float translationY = floatingView.getTranslationY();
@@ -197,7 +199,25 @@ public class RadioSwipeCardView extends RelativeLayout {
             floatingView.setTranslationX(translationX + x);
             floatingView.setTranslationY(translationY + y);
             floatingView.setRotation(10 * (sumX / widthRule));
-            floatingView.setAlpha(1.0f - 0.6f * Math.abs(sumY / heightRule));
+            floatingView.setAlpha(1.0f - 0.3f * Math.abs(sumY / heightRule));
+
+            float bottomViewRotation = BOTTOM_VIEW_ROTATION * (1.0f - Math.abs(sumDistanceY) / heightRule);
+            float bottomViewAlpha = Math.abs(sumDistanceY) / heightRule;
+            if (bottomViewAlpha < 0.0f) {
+                bottomViewAlpha = 0.0f;
+            } else if (bottomViewAlpha >= 1.0f) {
+                bottomViewAlpha = 1.0f;
+            }
+
+            if (bottomViewRotation < BOTTOM_VIEW_ROTATION) {
+                bottomViewRotation = BOTTOM_VIEW_ROTATION;
+            } else if (bottomViewRotation > 0) {
+                bottomViewRotation = 0;
+            }
+
+            mBottomCardView.setRotation(bottomViewRotation);
+            mBottomCardView.setAlpha(bottomViewAlpha);
+
 
         }
     }
@@ -207,45 +227,73 @@ public class RadioSwipeCardView extends RelativeLayout {
         if (decorView != null && floatingView != null) {
             AnimatorSet animationSet = new AnimatorSet();
 
-            animationSet.addListener(new Animator.AnimatorListener() {
-                @Override
-                public void onAnimationStart(Animator animation) {
-                    Log.i("chunyu", "onAnimationStart");
-                }
-
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    removeFloatingView();
-                    Log.i("chunyu", "onAnimationEnd");
-                }
-
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                    removeFloatingView();
-
-                    Log.i(" chunyu", "onAinmationCancel");
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animation) {
-
-                }
-            });
-
             if (Math.abs(sumDistanceX) < 100 && Math.abs(sumDistanceY) < 100) {
+
+                animationSet.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Log.i("chunyu", "onAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        removeFloatingViewA();
+                        Log.i("chunyu", "onAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        removeFloatingViewA();
+                        Log.i(" chunyu", "onAinmationCancel");
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+
                 animationSet.playTogether(
                         ObjectAnimator.ofFloat(floatingView, "rotation", floatingView.getRotation(), 0),
                         ObjectAnimator.ofFloat(floatingView, "translationX", floatingView.getTranslationX(), mTopCardViewX),
                         ObjectAnimator.ofFloat(floatingView, "translationY", floatingView.getTranslationY(), mTopCardViewY),
-                        ObjectAnimator.ofFloat(floatingView, "alpha", floatingView.getAlpha(), 1.0f));
+                        ObjectAnimator.ofFloat(floatingView, "alpha", floatingView.getAlpha(), 1.0f),
+                        ObjectAnimator.ofFloat(mBottomCardView, "rotation", mBottomCardView.getRotation(), BOTTOM_VIEW_ROTATION));
+
                 animationSet.setDuration(200).start();
             } else {
+                animationSet.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Log.i("chunyu", "onAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        removeFloatingViewB();
+                        Log.i("chunyu", "onAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        removeFloatingViewB();
+
+                        Log.i(" chunyu", "onAinmationCancel");
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+
+                renderView();
                 animationSet.playTogether(
                         ObjectAnimator.ofFloat(floatingView, "rotation", floatingView.getRotation(), floatingView.getRotation() + (float) (10 * getcosValue(sumDistanceX, sumDistanceY))),
                         ObjectAnimator.ofFloat(floatingView, "translationX", floatingView.getTranslationX(), floatingView.getTranslationX() + 200.0f * (float) getcosValue(sumDistanceX, sumDistanceY)),
                         ObjectAnimator.ofFloat(floatingView, "translationY", floatingView.getTranslationY(), floatingView.getTranslationY() + 200.0f * (float) getsinValue(sumDistanceX, sumDistanceY)),
-                        ObjectAnimator.ofFloat(floatingView, "alpha", floatingView.getAlpha(), 0.0f)
-                );
+                        ObjectAnimator.ofFloat(floatingView, "alpha", floatingView.getAlpha(), 0.0f),
+                        ObjectAnimator.ofFloat(mBottomCardView, "rotation", mBottomCardView.getRotation(), 0.0f));
 
                 animationSet.setDuration(300).start();
             }
@@ -259,6 +307,8 @@ public class RadioSwipeCardView extends RelativeLayout {
         TouchY = -1;
         sumDistanceY = -1;
         sumDistanceX = -1;
+        mTopCardViewX = -1;
+        mTopCardViewY = -1;
         lastX = -1;
         lastY = -1;
     }
@@ -272,11 +322,31 @@ public class RadioSwipeCardView extends RelativeLayout {
     }
 
 
-    private void removeFloatingView() {
+    private void removeFloatingViewA() {
         if (hasFloatingView && decorView != null) {
+            mTopCardView.setVisibility(View.VISIBLE);
+            mBottomCardView.setAlpha(0.0f);
+            mBottomCardView.setRotation(BOTTOM_VIEW_ROTATION);
             decorView.removeView(floatingView);
             hasFloatingView = false;
         }
+    }
+
+
+    private void removeFloatingViewB() {
+        if (hasFloatingView && decorView != null) {
+            mTopCardView.setVisibility(View.VISIBLE);
+            mBottomCardView.setRotation(BOTTOM_VIEW_ROTATION);
+            mBottomCardView.setAlpha(0.0f);
+            decorView.removeView(floatingView);
+            mBottomCardView.setBackground(new ColorDrawable(DemoUtils.getRandomColor()));
+            hasFloatingView = false;
+        }
+    }
+
+    private void renderView() {
+        Drawable colordrawable = mBottomCardView.getBackground();
+        mTopCardView.setBackground(colordrawable);
     }
 
     @Nullable
@@ -289,26 +359,31 @@ public class RadioSwipeCardView extends RelativeLayout {
     }
 
     @Nullable
-    public Bitmap createViewDrawable(View view) {
-        view.setDrawingCacheEnabled(true);
-        return Bitmap.createBitmap(view.getDrawingCache());
+    public Bitmap createViewDrawable() {
+        mTopCardView.setDrawingCacheEnabled(true);
+        mTopCardView.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(mTopCardView.getDrawingCache());
+        //这里要销毁一下drawing 缓存，因为如果不销毁，当下次在getDrawingCache()的时候，拿的有可能是旧的缓存。
+        mTopCardView.destroyDrawingCache();
+        return bitmap;
     }
 
 
     public void addViewToDecorView(View view) {
 
         if (decorView != null && !hasFloatingView) {
-            ViewGroup viewGroup = getDecorView((Activity) getContext());
+            ViewGroup viewGroup = getDecorView(getContext());
             floatingView = new ImageView(getContext());
             FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             floatingView.setLayoutParams(layoutParams);
-            floatingView.setImageBitmap(createViewDrawable(view));
+            floatingView.setImageBitmap(createViewDrawable());
             viewGroup.addView(floatingView);
 
             int xy[] = new int[2];
             view.getLocationOnScreen(xy);
             floatingView.setTranslationY(xy[1]);
             floatingView.setTranslationX(xy[0]);
+            mTopCardView.setVisibility(View.INVISIBLE);
             hasFloatingView = true;
         }
     }
